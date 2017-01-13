5e42e20f267d55d93554f65febb28a2dec7daec8
Dan Coombs <danny.coombs4@gmail.com>
2017-01-12 20:45:13 -0600

    first full test

--- setup_mount.sh
+++ setup_mount.sh
@@ -44,27 +44,27 @@ e2label $DEVICE permaroot
 tune2fs $DEVICE -U `uuidgen`
 mkdir $NEWMNT
 
-#
+
 # point of no return... 
 # modify /sbin/init on the ephemeral volume to chain-load from the persistent EBS volume, and then reboot.
-#
-# mv /sbin/init /sbin/init.backup
-# cat >/sbin/init <<EOF
-# #!/bin/sh
-# mount $DEVICE $NEWMNT
-# [ ! -d $NEWMNT/$OLDMNT ] && mkdir -p $NEWMNT/$OLDMNT
 
-# cd $NEWMNT
-# pivot_root . ./$OLDMNT
+mv /sbin/init /sbin/init.backup
+cat >/sbin/init <<EOF
+#!/bin/sh
+mount $DEVICE $NEWMNT
+[ ! -d $NEWMNT/$OLDMNT ] && mkdir -p $NEWMNT/$OLDMNT
 
-# for dir in /dev /proc /sys /run; do
-#     echo "Moving mounted file system ${OLDMNT}\${dir} to \$dir."
-#     mount --move ./${OLDMNT}\${dir} \${dir}
-# done
-# exec chroot . /sbin/init
-# EOF
-# chmod +x /sbin/init
-# shutdown -r now
+cd $NEWMNT
+pivot_root . ./$OLDMNT
+
+for dir in /dev /proc /sys /run; do
+    echo "Moving mounted file system ${OLDMNT}\${dir} to \$dir."
+    mount --move ./${OLDMNT}\${dir} \${dir}
+done
+exec chroot . /sbin/init
+EOF
+chmod +x /sbin/init
+shutdown -r now
 
 echo "END MOUNT" >> /root/user_data_mount
 echo "END MOUNT"
